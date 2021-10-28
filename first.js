d5cf78636bd9e46500f2dd873c041bc904cb7436
ashwinighodake <ashwini752000@gmail.com>
2021-10-28 14:46:34 +0530

    Updated first_javascript.html

--- first.js
+++ first.js
@@ -1,4 +1,8 @@
 function myfunction()
 {
-    document.write("Hello");
+    var uname=document.getElementById("input-box1").value;
+    var pass=document.getElementById("input-box2").value;
+
+    document.write("Username is:"+uname+"  "+"Password:"+pass);
+
 }
